832cad3376aa827aea68a8dff3d4a2a9039a9fde
thegreatestacc <67763738+thegreatestacc@users.noreply.github.com>
2020-08-27 00:00:19 +0300

    commit

---
+++ src/test/java/net/sovliv/shop_demo/ShopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sovliv.shop_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
